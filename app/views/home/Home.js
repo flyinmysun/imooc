2abb09de458a9eaab2bf62d94b349cd37a2f8b17
zww <wenwei.zhan@bestgym.cn>
2017-09-06 14:21:13 +0800

    初始化项目

--- app/views/home/Home.js
+++ app/views/home/Home.js
@@ -11,27 +11,22 @@ import {
     Image,
     Alert
 } from 'react-native';
+import Slider from './components/Slider'
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import ViewPager from 'react-native-viewpager';
+
 var {width,height} = Dimensions.get('window');
 
-var IMGS = [
+const advImgs = [
     'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1495271287&di=91be6b4c4a68b5523577f2cf2d8a30d0&src=http://pic2.ooopic.com/13/58/87/63bOOOPICb3_1024.jpg',
     'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1495271287&di=91be6b4c4a68b5523577f2cf2d8a30d0&src=http://pic2.ooopic.com/13/58/87/63bOOOPICb3_1024.jpg',
-
 ];
 
 export default class Home extends React.Component{
     constructor(props) {
         super(props);
 
-        var dataSource = new ViewPager.DataSource({
-            pageHasChanged: (p1, p2) => p1 !== p2,
-        });
-
         this.state = {
-            dataSource: dataSource.cloneWithPages(IMGS),
             iconNavDatas:[{id:1,color:'#FFBA5B',label:'实战'},
                 {id:2,color:'#3DDB9A',label:'猿问'},
                 {id:3,color:'#45C3FF',label:'手记'},
@@ -60,31 +55,22 @@ export default class Home extends React.Component{
 
     render(){
         return(
-            <View>
-                <FlatList
-                    data={[{id:1,name:"zww"},{id:2,name:"cy"}]}
-                    keyExtractor={this._keyExtractor}
-                    renderItem={this._renderItem}
-                    ListHeaderComponent={this._headerComponent}
-                    extraData={this.state}
-                />
-            </View>
+            <FlatList
+                data={[{id:1,name:"zww"},{id:2,name:"cy"}]}
+                keyExtractor={this._keyExtractor}
+                renderItem={this._renderItem}
+                ListHeaderComponent={this._headerComponent}
+                extraData={this.state}
+            />
         )
     }
 
     /**list 头部**/
     _headerComponent=()=>{
-
         return(
             <View>
-                <ViewPager
-                    style={{flex:1}}
-                    dataSource={this.state.dataSource}
-                    renderPage={this._renderPage}
-                    isLoop={true}
-                    autoPlay={true}
-                />
-                <View style={styles.iconNaWrap}>
+                <Slider sources={advImgs}/>
+                <View style={styles.iconNavWrap}>
                     {this.state.iconNavDatas.map((item,index)=>{
                         return(
                             <View key={index} style={styles.iconNavItemWrap}>
@@ -98,13 +84,6 @@ export default class Home extends React.Component{
                 </View>
             </View>
         )
-    }
-
-    /**广告**/
-    _renderPage = (data, pageID) => {
-        return (
-            <Image source={{uri: data}} style={styles.adImg} />
-        );
     }
 
     /**list**/
@@ -121,12 +100,7 @@ export default class Home extends React.Component{
 }
 
 var styles = StyleSheet.create({
-    adImg: {
-        width: width,
-        height:150,
-        resizeMode: 'stretch'
-    },
-    iconNaWrap:{
+    iconNavWrap:{
         flexDirection:'row',
         width:width,
         backgroundColor:"#fff",
